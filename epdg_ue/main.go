b42032f6027ba85070b46d7abdff2a83c7754a24
divvi96 <domenico.verde@nitel.it>
2024-02-16 10:07:40 +0100

    added results

--- epdg_ue/main.go
+++ epdg_ue/main.go
@@ -81,17 +81,43 @@ func main() {
 		securityAssociation := ikeMessage.Payloads.BuildSecurityAssociation()
 		proposal := securityAssociation.Proposals.BuildProposal(1, message.TypeIKE, nil)
 
-		/*var attributeType uint16 = message.AttributeTypeKeyLength
-		var keyLength uint16 = 256*/
-		proposal.EncryptionAlgorithm.BuildTransform(message.TypeEncryptionAlgorithm, message.ENCR_DES, nil, nil, nil)
+		// Encryption
+		var attributeType uint16 = message.AttributeTypeKeyLength
+		var keyLength256 uint16 = 256
+		var keyLength192 uint16 = 192
+		var keyLength128 uint16 = 128
+		proposal.EncryptionAlgorithm.BuildTransform(message.TypeEncryptionAlgorithm, message.ENCR_AES_CBC, &attributeType, &keyLength256, nil)
+		proposal.EncryptionAlgorithm.BuildTransform(message.TypeEncryptionAlgorithm, message.ENCR_AES_CBC, &attributeType, &keyLength192, nil)
+		proposal.EncryptionAlgorithm.BuildTransform(message.TypeEncryptionAlgorithm, message.ENCR_AES_CBC, &attributeType, &keyLength128, nil)
+		proposal.EncryptionAlgorithm.BuildTransform(message.TypeEncryptionAlgorithm, message.ENCR_AES_CTR, &attributeType, &keyLength256, nil)
+		proposal.EncryptionAlgorithm.BuildTransform(message.TypeEncryptionAlgorithm, message.ENCR_AES_CTR, &attributeType, &keyLength192, nil)
+		proposal.EncryptionAlgorithm.BuildTransform(message.TypeEncryptionAlgorithm, message.ENCR_AES_CTR, &attributeType, &keyLength128, nil)
+		proposal.EncryptionAlgorithm.BuildTransform(message.TypeEncryptionAlgorithm, message.ENCR_3DES, nil, nil, nil)
+		//proposal.EncryptionAlgorithm.BuildTransform(message.TypeEncryptionAlgorithm, message.ENCR_DES, nil, nil, nil)
+		// Integrity
 		proposal.IntegrityAlgorithm.BuildTransform(message.TypeIntegrityAlgorithm, message.AUTH_HMAC_SHA1_96, nil, nil, nil)
-		//proposal.IntegrityAlgorithm.BuildTransform(message.TypeIntegrityAlgorithm, message.AUTH_HMAC_MD5_96, nil, nil, nil)
-		proposal.PseudorandomFunction.BuildTransform(message.TypePseudorandomFunction, message.PRF_HMAC_SHA1, nil, nil, nil)
-		proposal.DiffieHellmanGroup.BuildTransform(message.TypeDiffieHellmanGroup, message.DH_1024_BIT_MODP, nil, nil, nil)
+		proposal.IntegrityAlgorithm.BuildTransform(message.TypeIntegrityAlgorithm, message.AUTH_HMAC_MD5_96, nil, nil, nil)
+		proposal.IntegrityAlgorithm.BuildTransform(message.TypeIntegrityAlgorithm, message.AUTH_HMAC_SHA2_256_128, nil, nil, nil)
+		proposal.IntegrityAlgorithm.BuildTransform(message.TypeIntegrityAlgorithm, message.AUTH_HMAC_SHA2_512_256, nil, nil, nil)
+		// PRF
+		proposal.PseudorandomFunction.BuildTransform(message.TypePseudorandomFunction, message.PRF_HMAC_MD5, nil, nil, nil)
+		//proposal.PseudorandomFunction.BuildTransform(message.TypePseudorandomFunction, message.PRF_HMAC_SHA1, nil, nil, nil)
+		//proposal.PseudorandomFunction.BuildTransform(message.TypePseudorandomFunction, message.PRF_HMAC_SHA2_256, nil, nil, nil)
+		//proposal.PseudorandomFunction.BuildTransform(message.TypePseudorandomFunction, message.PRF_HMAC_SHA2_384, nil, nil, nil)
+		//proposal.PseudorandomFunction.BuildTransform(message.TypePseudorandomFunction, message.PRF_HMAC_SHA2_512, nil, nil, nil)
+		// DH
+		//proposal.DiffieHellmanGroup.BuildTransform(message.TypeDiffieHellmanGroup, message.DH_768_BIT_MODP, nil, nil, nil)
+		//proposal.DiffieHellmanGroup.BuildTransform(message.TypeDiffieHellmanGroup, message.DH_1024_BIT_MODP, nil, nil, nil)
+		//proposal.DiffieHellmanGroup.BuildTransform(message.TypeDiffieHellmanGroup, message.DH_1536_BIT_MODP, nil, nil, nil)
+		proposal.DiffieHellmanGroup.BuildTransform(message.TypeDiffieHellmanGroup, message.DH_2048_BIT_MODP, nil, nil, nil)
+		//proposal.DiffieHellmanGroup.BuildTransform(message.TypeDiffieHellmanGroup, message.DH_3072_BIT_MODP, nil, nil, nil)
+		//proposal.DiffieHellmanGroup.BuildTransform(message.TypeDiffieHellmanGroup, message.DH_4096_BIT_MODP, nil, nil, nil)
+		//proposal.DiffieHellmanGroup.BuildTransform(message.TypeDiffieHellmanGroup, message.DH_6144_BIT_MODP, nil, nil, nil)
+		//proposal.DiffieHellmanGroup.BuildTransform(message.TypeDiffieHellmanGroup, message.DH_8192_BIT_MODP, nil, nil, nil)
 
 		// Key exchange data
-		generator := new(big.Int).SetUint64(handler.Group2Generator)
-		factor, ok := new(big.Int).SetString(handler.Group2PrimeString, 16)
+		generator := new(big.Int).SetUint64(handler.Group14Generator)
+		factor, ok := new(big.Int).SetString(handler.Group14PrimeString, 16)
 		if !ok {
 			fmt.Errorf("Generate key exchange data failed")
 		}
@@ -99,7 +125,7 @@ func main() {
 		localPublicKeyExchangeValue := new(big.Int).Exp(generator, secret, factor).Bytes()
 		prependZero := make([]byte, len(factor.Bytes())-len(localPublicKeyExchangeValue))
 		localPublicKeyExchangeValue = append(prependZero, localPublicKeyExchangeValue...)
-		ikeMessage.Payloads.BUildKeyExchange(message.DH_1024_BIT_MODP, localPublicKeyExchangeValue)
+		ikeMessage.Payloads.BUildKeyExchange(message.DH_2048_BIT_MODP, localPublicKeyExchangeValue)
 
 		// Nonce
 		localNonce := handler.GenerateRandomNumber().Bytes()
@@ -237,7 +263,7 @@ func main() {
 					mapEpdgReply[epdgUDPAddr.String()] = "no-proposal-chosen"
 					break
 				} else if notification.NotifyMessageType == message.INVALID_SYNTAX {
-					fmt.Println("Received Invalid Syntax Payload. Try to change Proposal")
+					fmt.Println("Received Invalid Syntax Payload.")
 					mapEpdgReply[epdgUDPAddr.String()] = "invalid-syntax"
 					break
 				} else if notification.NotifyMessageType == message.INVALID_KE_PAYLOAD {
